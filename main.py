396c9018a630856d372ca6f89773ecd0542a78ca
Ariela Deleon <ArielaDeleon2020@u.northwestern.edu>
2019-01-25 16:59:47 -0600

    message

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ class SetupTest(unittest.TestCase):
 		result = student_code.shouldReturnTrue()
 		self.assertTrue(result)
 
+	def test02(self):
+		result = student_code.shouldReturnTrue()
+		self.assertEqual(result, True)
+
 if __name__ == '__main__':
 	unittest.main()
